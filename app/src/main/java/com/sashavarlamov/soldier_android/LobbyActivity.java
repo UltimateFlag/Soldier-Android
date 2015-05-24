9fddba25677bff156962aeb8aa13dd11dbdc24cc
Sasha Varlamov <sasha@sashavarlamov.com>
2015-05-24 03:57:40 -0400

    Tying data to view

--- app/src/main/java/com/sashavarlamov/soldier_android/LobbyActivity.java
+++ app/src/main/java/com/sashavarlamov/soldier_android/LobbyActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.github.nkzawa.emitter.Emitter;
 
@@ -16,11 +17,12 @@ public class LobbyActivity extends ActionBarActivity {
     private int spectators = 0;
     private int teamOneCount = 0;
     private int teamTwoCount = 0;
+    private Intent intent = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
+        intent = getIntent();
         username = intent.getStringExtra(username);
         setContentView(R.layout.activity_lobby);
         SocketUtil.onTeamSwitched(onTeamSwitched);
@@ -32,20 +34,32 @@ public class LobbyActivity extends ActionBarActivity {
         teamTwoName = intent.getStringExtra("teamTwoName");
         teamTwoCount = intent.getIntExtra("teamTwoCount", 0);
         this.setTitle(intent.getStringExtra("gameName") + " - Lobby");
-        ((Button)findViewById(R.id.choose_team_one)).setText(teamOneName + " (" + teamOneCount + ")");
-        ((Button)findViewById(R.id.choose_team_two)).setText(teamTwoName + " (" + teamTwoCount + ")");
+        ((ToggleButton)findViewById(R.id.choose_team_one)).setText(intent.getStringExtra("teamOneName") + " (" + teamOneCount + ")");
+        ((ToggleButton)findViewById(R.id.choose_team_two)).setText(intent.getStringExtra("teamTwoName") + " (" + teamOneCount + ")");
     }
 
     public void teamOneSelected(View view){
         // TODO: Select a team
         System.out.println(getIntent().getStringExtra("teamOneName") + " was selected");
         SocketUtil.switchTeams(0);
+        if(!((ToggleButton)findViewById(R.id.choose_team_one)).isChecked())
+            ((ToggleButton)findViewById(R.id.choose_team_one)).setChecked(true);
+        if(((ToggleButton)findViewById(R.id.choose_team_two)).isChecked())
+            ((ToggleButton)findViewById(R.id.choose_team_two)).setChecked(false);
+        ((ToggleButton)findViewById(R.id.choose_team_one)).setText(intent.getStringExtra("teamOneName") + " (" + teamOneCount + ")");
+        ((ToggleButton)findViewById(R.id.choose_team_two)).setText(intent.getStringExtra("teamTwoName") + " (" + teamOneCount + ")");
     }
 
     public void teamTwoSelected(View view){
         // TODO: Select a team
         System.out.println(getIntent().getStringExtra("teamTwoName") + " was selected");
         SocketUtil.switchTeams(1);
+        if(((ToggleButton)findViewById(R.id.choose_team_one)).isChecked())
+            ((ToggleButton)findViewById(R.id.choose_team_one)).setChecked(false);
+        if(!((ToggleButton)findViewById(R.id.choose_team_two)).isChecked())
+            ((ToggleButton)findViewById(R.id.choose_team_two)).setChecked(true);
+        ((ToggleButton)findViewById(R.id.choose_team_one)).setText(intent.getStringExtra("teamOneName") + " (" + teamOneCount + ")");
+        ((ToggleButton)findViewById(R.id.choose_team_two)).setText(intent.getStringExtra("teamTwoName") + " (" + teamOneCount + ")");
     }
 
     private Emitter.Listener onTeamSwitched = new Emitter.Listener() {
